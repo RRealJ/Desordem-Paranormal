349cf15922535302fd609b303861f1c8bc91395f
Renato Real <90974187+RRealJ@users.noreply.github.com>
2025-07-23 19:48:17 -0300

    added sprite and weapon
    Added JoJo Sprite,
    Added Main Weapon,
    Added bullet scene,
    Modified Resources.

--- desordem-paranormal/Player/player.gd
+++ desordem-paranormal/Player/player.gd
@@ -3,9 +3,22 @@ extends CharacterBody2D
 
 @export var health: int = 50
 @export var speed: float = 100
+@export var weapon_stats: Weapon_stats
+@export var bullet: PackedScene
+
+var main_weapon: Node2D
+var shooting: bool = false
+var weapon_cooldown: Timer
+
+func _ready() -> void:
+	main_weapon = $main_weapon
+	weapon_cooldown = $weapon_cooldown
+	weapon_cooldown.wait_time = weapon_stats.attack_cooldown
+	$Bottom.play("Walking")
 
 
 func _process(delta: float) -> void:
+		
 	var direction:Vector2 = Vector2.ZERO
 	
 	if Input.is_action_pressed("MoveUp"):
@@ -21,3 +34,18 @@ func _process(delta: float) -> void:
 
 	velocity = direction * speed
 	move_and_slide()
+	
+	
+func shoot():
+	$Upper.play("Attack")
+	shooting = true
+	
+	var instance = bullet.instantiate()
+	get_tree().root.add_child(instance)
+	instance.global_position = global_position
+	instance.rotation = rotation
+	
+
+func _on_weapon_cooldown_timeout() -> void:
+	shooting = false
+	shoot()
