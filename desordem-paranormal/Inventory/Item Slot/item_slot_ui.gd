87c73e60a7bc421f20d16c1a83aeab8e93c173c3
Renato Real <90974187+RRealJ@users.noreply.github.com>
2025-07-31 15:33:01 -0300

    inventory and item system completed
    added new placeholder image for items
    added insertion of items in certain slots in the inventory ui

--- desordem-paranormal/Inventory/Item Slot/item_slot_ui.gd
+++ desordem-paranormal/Inventory/Item Slot/item_slot_ui.gd
@@ -1,43 +1,38 @@
 extends Control
 
 @export var item_res: Resource
-@onready var rich_text: RichTextLabel = $Panel/HBoxContainer/RichTextLabel
-@onready var panel: Panel = $Panel
-@onready var portrait: TextureRect = $Panel/HBoxContainer/TextureRect
+@onready var rich_text: RichTextLabel = $HBoxContainer/RichTextLabel
+@onready var portrait: TextureRect = $HBoxContainer/TextureRect
+@onready var level_label: Label = $HBoxContainer/TextureRect/Label
 
 const COLOR_BLOOD: Color = Color(0.66, 0.08, 0.0, 1)
 const COLOR_DEATH: Color = Color(0.59, 0.6, 0.59, 1)
 const COLOR_ENERGY: Color = Color(0.67, 0.29, 1, 1)
 const COLOR_KNOWLEDGE: Color = Color(0.93, 0.71, 0.0, 1)
 
-func _ready() -> void:
+
+func update_item_slot() -> void:
 	var new_text: String
-	var text_exemple: String = "A Katana de Sangue com a trilha da morte, enfraquece o conhecimento e recusa a Energia"
-	
-	#new_text = highlight_word(item_res.description)
-	new_text = highlight_word(text_exemple)
+	new_text = highlight_word(item_res.description)
 	rich_text.text = new_text
 	
-	#new_texture =
-	#portrait = 
+	level_label.text = str(item_res.level)
+	update_portrait(item_res.portrait_path)
 	
-	var element:int = 0
-
-	#match item_res.element_type:
-	match element:
+	match item_res.element_type:
 		0:
-			panel.self_modulate = COLOR_BLOOD
+			self_modulate = COLOR_BLOOD
 		1: 
-			panel.self_modulate = COLOR_DEATH
+			self_modulate = COLOR_DEATH
 		2:
-			panel.self_modulate = COLOR_ENERGY
+			self_modulate = COLOR_ENERGY
 		3:
-			panel.self_modulate = COLOR_KNOWLEDGE
+			self_modulate = COLOR_KNOWLEDGE
 		4: 
 			pass
 		_:
-			panel.self_modulate = Color(0.93, 0.71, 0.0, 0)
-	
+			self_modulate = Color(0.93, 0.71, 0.0, 0)
+
 
 func highlight_word(text: String) -> String:
 	
@@ -56,3 +51,12 @@ func highlight_word(text: String) -> String:
 	text = text.replace("Conhecimento", "[color=%s]%s[/color]" % [COLOR_KNOWLEDGE.to_html(), "conhecimento".capitalize()])
 				
 	return text
+
+
+func update_portrait(new_texture: String) -> void:
+	var image: Texture2D = load(new_texture) as Texture2D
+	portrait.texture = image
+
+
+func _on_button_mouse_entered() -> void:
+	$Button.grab_focus()
--- desordem-paranormal/Inventory/inventory_control.gd
+++ desordem-paranormal/Inventory/inventory_control.gd
@@ -2,20 +2,18 @@ extends Control
 
 #funcionando, porém nao vai mostrar o item no inspector
 @export var inventory: Inventory
-@onready var item_slot_ui: PackedScene = preload("res://Inventory/Item Slot/item_slot_ui.tscn")
+@onready var item_slots_ui: Array[Node] = $"Panel/VScrollContainer/VBoxContainer".get_children()
 @onready var stage: Node2D = $"../.."
 
-var player: CharacterBody2D
-
 func _ready() -> void:
-	player = Global.player
 	inventory.item_slots.clear()
 	inventory.item_slots.resize(10)
-	insert_item(player.main_weapon.weapon_stats.id)
+	#insert_item(Global.player.main_weapon.weapon_stats.id) #for add main weapon of character
 
 
 func insert_item(item_id: int) -> void:
 	var item_found: Resource
+	var index_slot: int = -1
 	
 	for item in Global.items_resources_avaible: # {resource.id : resource}
 		item_found = item.get(item_id)
@@ -26,16 +24,28 @@ func insert_item(item_id: int) -> void:
 				if inventory.item_slots[i]  == null:
 					inventory.item_slots[i] = Item_slot.new()
 					inventory.item_slots[i].item = item_found
-					break
+					index_slot = i
 				
 				else:
 					if inventory.item_slots[i].item == item_found:
 						inventory.item_slots[i].item.level += 1
-						break
+						index_slot = i
 						
-			break
+				break	
+		
+		update_item_slots_ui(index_slot)
+		break
 
-func reset_items_level_and_clean_inventory() -> void:  #call when player life reaches 0
+
+func update_item_slots_ui(slot_index: int) -> void:
+	if slot_index != -1:
+		item_slots_ui[slot_index].item_res = inventory.item_slots[slot_index].item
+		item_slots_ui[slot_index].update_item_slot()
+
+
+func reset_items_level() -> void:  #call when player life reaches 0
 	for i in range(inventory.item_slots.size()):
 		if inventory.item_slots[i]  != null:
 			inventory.item_slots[i].item.level = 1
+			
+	
